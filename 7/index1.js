605d9346b1b28135c0603590db41a4b592489096
Gomozovghost <116781681+Gomozovghost@users.noreply.github.com>
2022-11-22 20:10:34 +0700

    Update index1.js

--- 7/index1.js
+++ 7/index1.js
@@ -1,9 +1,9 @@
-function Kartochka(KarzinaStoimost, PodschetHorosho, Talonchik = null) { let ObshayaStoimost = KarzinaStoimost;
-if (Talonchik = 'Промокод: ДАРИМ300' && KarzinaStoimost < 300) {ObshayaStoimost = 0; console.log('Промокод: ДАРИМ300 ' + ObshayaStoimost + '₽');}
-else if (Talonchik = 'Прокод: ДАРИМ300')
-{ObshayaStoimost -= 300; console.log('ДАРИМ300: ' + ObshayaStoimost + '₽');}
-if (PodschetHorosho >= 10) {const PodschetFiveProcent = KarzinaStoimost/100*5; ObshayaStoimost -= PodschetFiveProcent; console.log('Более 10 товаров: ' + ObshayaStoimost + '₽');}
+function Kartochka(KarzinaStoimost, Podschet, Talon = null) { let ObshayaStoimost = KarzinaStoimost;
+if (Talon = 'Промокод: Консервы' && KarzinaStoimost < 600) {ObshayaStoimost = 0; console.log('Промокод: Консервы' + ObshayaStoimost + '₽');}
+else if (Talon = 'Прокод: Консервы')
+{ObshayaStoimost -= 300; console.log('Консервы: ' + ObshayaStoimost + '₽');}
+if (Podschet >= 10) {const PodschetFiveProcent = KarzinaStoimost/100*5; ObshayaStoimost -= PodschetFiveProcent; console.log('Более 10 товаров: ' + ObshayaStoimost + '₽');}
 if (KarzinaStoimost > 50000) {const PodschetTwentyProcent = ObshayaStoimost/100*20; const PodschetDifference = ObshayaStoimost - 50000; ObshayaStoimost = PodschetDifference - PodschetTwentyProcent; console.log('Более 50 000 ₽: ' + ObshayaStoimost + '₽');}
-if (Talonchik = 'Акция: СКИДКА15' && KarzinaStoimost >= 20000) {const PodschetPedesaytProcent = ObshayaStoimost/100*15; ObshayaStoimost -= PodschetPedesaytProcent; console.log('Акция: СКИДКА15 ' + ObshayaStoimost +'₽'); }
+if (Talon = 'Акция: СКИДКА15' && KarzinaStoimost >= 20000) {const PodschetPedesaytProcent = ObshayaStoimost/100*15; ObshayaStoimost -= PodschetPedesaytProcent; console.log('Акция: СКИДКА15 ' + ObshayaStoimost +'₽'); }
 console.log('Стоимость товаров равна: ' + ObshayaStoimost + '₽');}
-Kartochka(200000, 16, 'Промокод: ДАРИМ300');
+Kartochka(200000, 16, 'Промокод: Консервы');
